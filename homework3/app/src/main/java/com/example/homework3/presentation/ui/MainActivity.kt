53814544e6a776e44a0e8fce832dc2a1da0b10ce
AntonLm423 <anton.kiselyov02@mail.ru>
2023-03-02 21:46:50 +0300

    homework3

---
+++ homework3/app/src/main/java/com/example/homework3/presentation/ui/MainActivity.kt
@@ -0,0 +1,29 @@
+package com.example.homework3.presentation.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.core.view.WindowCompat
+import androidx.fragment.app.FragmentActivity
+import com.example.homework3.R
+import androidx.fragment.app.add
+import androidx.fragment.app.commit
+import com.example.homework3.presentation.ui.signin.SignInFragment
+
+class MainActivity : FragmentActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        fitContentViewToInsets()
+
+        supportFragmentManager.commit() {
+                add<SignInFragment>(R.id.container)
+            }
+
+    }
+
+    private fun fitContentViewToInsets() {
+        WindowCompat.setDecorFitsSystemWindows(window, false)
+    }
+}
